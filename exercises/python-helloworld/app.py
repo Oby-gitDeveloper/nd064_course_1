df0e94c4575e528b2783a06e00db8f41abba1a71
Oby-gitDeveloper <69041949+Oby-gitDeveloper@users.noreply.github.com>
2021-09-05 00:59:58 +0100

    Update app.py
    Added status and metrics endpoints

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,37 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route("/status")
+def healthcheck():
+    response = app.response_class(
+            response=json.dumps({"result": "OK- healthy"})
+            status=200,
+            mimetype='application/json'
+    )
+    
+    app.logger.info('status request successfull') 
+    return response       
+            
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response=json.dumps({"status": "success", "code":"0", "data":{"UserCount":140,"UserCountActive":23}})
+        status=200,
+        mimetype='application/json'
+                            
+    )
+    app.logger.info('metrics request successfull')
+    return response 
+
+@app.route("/")
+def hello():
+    app.logger.info('Main request successful')
+    
 @app.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
+     ##stream logs to a file
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
     app.run(host='0.0.0.0')
